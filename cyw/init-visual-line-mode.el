d53c5850c5afeab2794533c4dff2c72b977c1bb1
magicianmartin <killerc@163.com>
2017-12-02 22:36:38 +0800

    设置编程模式时不要自动换行

--- cyw/init-visual-line-mode.el
+++ cyw/init-visual-line-mode.el
@@ -5,13 +5,11 @@
 ;; 全局启用visual-line-mode，视觉上的换行。
 ;; (global-visual-line-mode 1)
 
-;; (add-hook 'prog-mode-hook (lambda () (setq truncate-lines 1)))
+;; 设置编程模式时不要自动换行
+;; truncate-lines: Automatically becomes buffer-local when set.
+(add-hook 'prog-mode-hook (lambda () (setq truncate-lines 1)))
 
-
-;; (toggle-truncate-lines -1)
-
-(setq truncate-partial-width-windows 9999)
-(setq truncate-lines nil)
+(setq truncate-partial-width-windows nil)
 
 (provide 'init-visual-line-mode)
 ;;; init-visual-line-mode ends here
