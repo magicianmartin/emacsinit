da317395da11c98be3bb8b5c90b5693b2a8e5ac4
magicianmartin <killerc@163.com>
2018-01-14 17:10:43 +0800

    仅在window-system不为nil时才有截屏函数

--- cyw/init-custom.el
+++ cyw/init-custom.el
@@ -2,22 +2,25 @@
 ;;; Commentary:
 ;;; Code:
 
-
-
 ;; screenshot
-(defun chuyw-capture-screen()
-  ""
-  (interactive)
-  (lower-frame)
-  (setq img-name
-        (concat (make-temp-name "img-") ".png"))
-  (call-process "i_view32" nil nil nil (format "/capture=4 /convert=.\\img\\%s" img-name))
-  ;; (shell-command (concat "i_view32 /capture=4 /convert=" ".\\img\\" (format "%s" img-name)))
-  (insert (concat "[[./img/" img-name "]]"))
-  (shell-command (format "mspaint .\\img\\%s" img-name))
+(defun chuyw-init-capture-screen()
+  (defun chuyw-capture-screen()
+    ""
+    (interactive)
+    (lower-frame)
+    (setq img-name
+	  (concat (make-temp-name "img-") ".png"))
+    (call-process "i_view32" nil nil nil (format "/capture=4 /convert=.\\img\\%s" img-name))
+    ;; (shell-command (concat "i_view32 /capture=4 /convert=" ".\\img\\" (format "%s" img-name)))
+    (insert (concat "[[./img/" img-name "]]"))
+    (shell-command (format "mspaint .\\img\\%s" img-name))
+    )
+
+  (with-eval-after-load 'org (define-key org-mode-map (kbd "C-c r") 'chuyw-capture-screen))
   )
 
-(with-eval-after-load 'org (define-key org-mode-map (kbd "C-c r") 'chuyw-capture-screen))
+(when (not (eq nil window-system))
+  (chuyw-init-capture-screen))
 
 (provide 'init-custom)
 ;;; init-custom ends here
