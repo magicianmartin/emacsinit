3feb5aa7d9b221296afdb5881dc00be8a7e711df
martin <killerc@163.com>
2018-07-20 23:17:42 +0800

    add beamer

--- cyw/init-org.el
+++ cyw/init-org.el
@@ -21,5 +21,38 @@ urlcolor=blue,menucolor=blue,pdfstartview=FitH]{hyperref}"
                ("\\subparagraph{%s}" . "\\subparagraph*{%s}")
                ))
 
+;; from other
+;; (add-to-list 'org-latex-classes
+;;              ;; beamer class, for presentations
+;;              '("beamer"
+;;                "\\documentclass[11pt,professionalfonts]{beamer}
+;; \\mode
+;; \\usetheme{{{{Warsaw}}}}
+;; %\\usecolortheme{{{{beamercolortheme}}}}
+;; \\beamertemplateballitem
+;; \\setbeameroption{show notes}
+;; \\institute{{{{beamerinstitute}}}}
+;; \\subject{{{{beamersubject}}}}"
+;;                ("\\section{%s}" . "\\section*{%s}")
+;;                ("\\begin{frame}[fragile]\\frametitle{%s}"
+;;                 "\\end{frame}"
+;;                 "\\begin{frame}[fragile]\\frametitle{%s}"
+;;                 "\\end{frame}")))
+
+;; a simple beamer
+(add-to-list 'org-latex-classes
+             ;; beamer class, for presentations
+             '("beamer"
+               "\\documentclass[11pt,professionalfonts,UTF8]{beamer}
+\\mode<beamer>{\\usetheme[hideothersubsections,right,width=22mm]{PaloAlto}}
+\\beamertemplateballitem
+\\setbeameroption{show notes}
+\\institute{magician}
+\\subject{{{{beamersubject}}}}
+\\usepackage{ctex}"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\begin{frame}[fragile]\\frametitle{%s}"
+                "\\end{frame}")))
+
 (provide 'init-org)
 ;;; init-org ends here
