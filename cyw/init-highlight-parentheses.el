3c36fc73ff3f6254f3f2a2aec0cb7ae61270bcd4
magicianmartin <killerc@163.com>
2019-06-08 08:35:13 +0800

    删除无用代码

--- cyw/init-highlight-parentheses.el
+++ cyw/init-highlight-parentheses.el
@@ -10,9 +10,6 @@
   (setq hl-paren-background-colors '("Yellow"))
   (add-hook 'prog-mode-hook (lambda () (highlight-parentheses-mode 1)))
   )
-;; (add-to-list 'prog-mode-hook (lambda () (highlight-parentheses-mode 1)))
-
-;; (setq hl-paren-colors '("Green" "Orange" "Yellow" "Red"))
 
 (provide 'init-highlight-parentheses)
 ;;; init-highlight-parentheses ends here
