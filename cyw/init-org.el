fa3d9d76d0aece7b82313a95997ae40c057008f3
magicianmartin <killerc@163.com>
2019-06-08 18:20:36 +0800

    使用org生成latex并生成PDF文件。

--- cyw/init-org.el
+++ cyw/init-org.el
@@ -3,10 +3,6 @@
 ;;; Code:
 
 
-;; orgmode中文本语法高亮
-;; (setq org-src-fontify-natively t)
-;; (with-eval-after-load 'org (add-to-list 'org-mode-hook (lambda () (setq truncate-lines nil))))
-
 (use-package org
   :config
   ;; 代码块语法高亮
@@ -16,40 +12,40 @@
   )
 
 ;; 导出PDF的设置
+;; 生成PDF文件需要使用到 xelatex 程序，安装了 texlive 后就有了 xelatex 程序。完全安装 texlive 可以使用如下指令：
+;; sudo dnf install texlive-scheme-full
 (use-package ox-latex
   :after (org)
   :config
-;; (require 'ox-latex)
+  ;; (require 'ox-latex)
+  (setq org-latex-compiler "xelatex")
   (add-to-list 'org-latex-classes
-               '("ctexart"
-                 ;; "\\documentclass[11pt, hyperref, UTF8, a4paper]{ctexart}
-                 "\\documentclass[hyperref, UTF8, a4paper]{ctexart}
-\\usepackage[colorlinks=true,CJKbookmarks=false,linkcolor=blue,
-urlcolor=blue,menucolor=blue,pdfstartview=FitH]{hyperref}"
-                 ("\\section{%s}" . "\\section*{%s}")
+               '("cxeart"
+                 "\\documentclass{article}
+\\usepackage{xeCJK}%使用xeCJK宏包处理中文
+\\usepackage[colorlinks=true,linkcolor=blue,urlcolor=blue,menucolor=blue,%超链接等使用蓝色
+pdfstartview=FitH]{hyperref}%默认输出的PDF文件打开时为适合宽度
+\\usepackage{indentfirst}%段首缩进需要的宏包
+\\setlength{\\parindent}{2em}%设置段首缩进两个汉字"
+                ("\\section{%s}" . "\\section*{%s}")
                  ("\\subsection{%s}" . "\\subsection*{%s}")
                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
                  ("\\paragraph{%s}" . "\\paragraph*{%s}")
                  ("\\subparagraph{%s}" . "\\subparagraph*{%s}")
                  ))
 
-  ;; from other
-  ;; (add-to-list 'org-latex-classes
-  ;;              ;; beamer class, for presentations
-  ;;              '("beamer"
-  ;;                "\\documentclass[11pt,professionalfonts]{beamer}
-  ;; \\mode
-  ;; \\usetheme{{{{Warsaw}}}}
-  ;; %\\usecolortheme{{{{beamercolortheme}}}}
-  ;; \\beamertemplateballitem
-  ;; \\setbeameroption{show notes}
-  ;; \\institute{{{{beamerinstitute}}}}
-  ;; \\subject{{{{beamersubject}}}}"
-  ;;                ("\\section{%s}" . "\\section*{%s}")
-  ;;                ("\\begin{frame}[fragile]\\frametitle{%s}"
-  ;;                 "\\end{frame}"
-  ;;                 "\\begin{frame}[fragile]\\frametitle{%s}"
-  ;;                 "\\end{frame}")))
+  (setq org-latex-default-class "cxeart")
+;;   (add-to-list 'org-latex-classes
+;;                '("ctexart"
+;;                  "\\documentclass[hyperref, UTF8, a4paper]{ctexart}
+;; \\usepackage[colorlinks=true,CJKbookmarks=false,linkcolor=blue,
+;; urlcolor=blue,menucolor=blue,pdfstartview=FitH]{hyperref}"
+;;                  ("\\section{%s}" . "\\section*{%s}")
+;;                  ("\\subsection{%s}" . "\\subsection*{%s}")
+;;                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+;;                  ("\\paragraph{%s}" . "\\paragraph*{%s}")
+;;                  ("\\subparagraph{%s}" . "\\subparagraph*{%s}")
+;;                  ))
 
   ;; a simple beamer
   (add-to-list 'org-latex-classes
@@ -61,7 +57,7 @@ urlcolor=blue,menucolor=blue,pdfstartview=FitH]{hyperref}"
 \\setbeameroption{show notes}
 \\institute{magician}
 \\subject{{{{beamersubject}}}}
-\\usepackage{ctex}"
+\\usepackage{xeCJK}"
                  ("\\section{%s}" . "\\section*{%s}")
                  ("\\begin{frame}[fragile]\\frametitle{%s}"
                   "\\end{frame}")))
